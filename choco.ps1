9fedb379c7e3d7318407d2ac13a250b7e1231ea3
Sergey Zubkov <ksevelyar@gmail.com>
2020-10-23 11:19:13 +0300

    Update choco.ps1

--- choco.ps1
+++ choco.ps1
@@ -2,6 +2,7 @@
 # powershell.exe D:\choco.ps1
 
 Update-ExecutionPolicy Unrestricted
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # 🍫 apps
 choco install chocolatey-core.extension chocolateygui -y
